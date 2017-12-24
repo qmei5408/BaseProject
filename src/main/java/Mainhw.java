7490a871327ed8e44d8903370a4e595bacf6845d
fcu-d0530299 <32416492+fcu-d0530299@users.noreply.github.com>
2017-12-24 23:04:38 +0800

    Homework program

---
+++ src/main/java/Mainhw.java
@@ -0,0 +1,62 @@
+package homework;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class Mainhw {
+
+  public static void main(String[] args) throws ParseException, IOException {
+    Scanner scanner = new Scanner(System.in);
+
+    Time time = new Time();
+    Record record = new Record();
+    
+    
+    boolean test = true;
+    while (test) {
+      System.out.println("1.Add homework.");
+      System.out.println("2.Delete homework.");
+      System.out.println("3.View homework.");
+      System.out.println("Exit. Any key.");
+      int choose = scanner.nextInt();
+      
+      switch (choose) {
+        case 1 :
+          scanner.nextLine();
+          System.out.println("Input Subject name.");
+          String name = scanner.nextLine();
+          System.out.println("Input homework information.");
+          String note = scanner.nextLine();
+    
+          while (true) {
+            System.out.println("Input time limit.(yyyy-MM-dd)");
+            String date = scanner.nextLine();
+            if (time.checkTime(date)) {
+              record.add(name, date, note);
+              break;
+            } else {
+              System.out.println("Time wrong, input again.");
+            }
+          }
+          System.out.println("Done add");
+          break;
+        case 2 :
+          record.del();
+          break;
+        case 3 :
+          System.out.println("1.View all.");
+          System.out.println("2.View the deadline is coming.");
+          int input = scanner.nextInt();
+          record.read(input);
+          break;
+        default:
+          test = false;
+          break;
+      }
+    }
+
+    scanner.close();
+  }
+
+}
--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -1,16 +1,55 @@
+package homework;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Time {
+  SimpleDateFormat nowdate = new java.text.SimpleDateFormat("yyyy-MM-dd"); 
+  
+  private String sdate;
 
-    public static void getDate(){
-        Date date = new Date();
-        SimpleDateFormat fmt = new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a");
-        System.out.println("The time now is : " + fmt.format(date));
+  public Time() {
+    nowdate.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+    sdate = nowdate.format(new java.util.Date());
+    
+  }
+
+  public boolean checkTime(String date) throws ParseException {
+    Date date1 = nowdate.parse(sdate);
+    Date date2 = nowdate.parse(date);
+    
+    if (date1.compareTo(date2) != 1) {
+      return true;
     }
+    
+    return false;
+  }
+  
 
-//    public void errorChecking(){
-//
-//    }
+  public boolean timeComing(String date) throws ParseException {
+    Date date1 = nowdate.parse(sdate);
+    Date date2 = nowdate.parse(date);
+    
+    long day = (date2.getTime() - date1.getTime()) / (24 * 60 * 60 * 1000); 
+    if (day <= 3) {
+      return true;
+    }
+    
+    return false;
+  }
+  
+
+  public boolean timeDele(String time1, String time2) throws ParseException {
+    Date date1 = nowdate.parse(time1);
+    Date date2 = nowdate.parse(time2);
+    
+    long day = (date2.getTime() - date1.getTime()) / (24 * 60 * 60 * 1000); 
+    if (day == 0) {
+      return true;
+    }
+    
+    return false;
+  }
 }
